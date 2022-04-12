9936a4bd8071444cd8eec5824f635e38ac277d1d
Emlyn Matheson <emlynmatheson@gmail.com>
2022-04-12 23:59:12 +1200

    Add mark.js

---
+++ docs/mark.js
@@ -0,0 +1,48 @@
+function testDataInput(elem) {
+
+	var classIdx = $(elem).attr('class').split("-")[1];
+	var allInRow = $(".testData-" + classIdx);
+
+	var maxClassIdx = classIdx;
+	for (var sib in $(elem).siblings()) {
+
+		if (isNaN(sib)) { continue; }
+
+		var sibClassIdx = $($(elem).siblings()[sib]).attr('class').split('-')[1];
+		if (sibClassIdx > maxClassIdx) { maxClassIdx = sibClassIdx; }
+	}
+
+
+	var allEmpty = true;
+	for (rowElem in allInRow) {
+		//Check if is a 'proper' key (hacky workaround)
+		if (isNaN(rowElem)) { continue; }
+
+		if (allInRow[rowElem].value != "") {
+			allEmpty = false;
+			break;
+		}
+	}
+
+	if (allEmpty) {
+		if ($(elem).siblings().length > 0) {
+			if (allEmpty) {
+				for (rowElem in allInRow) {
+					//Check if is a 'proper' key
+					if (isNaN(rowElem)) { continue; }
+					allInRow[rowElem].remove();
+				}
+			}
+		}
+
+	} else {
+		if (maxClassIdx == classIdx) {
+			//Create new (in both cols)
+			for (rowElem in allInRow) {
+				if (isNaN(rowElem)) { continue; }
+				console.log("bleh");
+				$(allInRow[rowElem]).clone().attr('class', 'testData-'+(classIdx+1)).insertAfter($(allInRow[rowElem]));
+			}
+		}
+	}
+}
